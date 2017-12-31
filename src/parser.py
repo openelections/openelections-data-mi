cc54bf389c8253bc3ba6390b4df7a91396d0d36f
Nick Kocharhook <nick@kocharhook.com>
2017-12-31 16:48:10 +0000

    [#2] Create separate files for special and general elections held on the same day. Properly format candidate names and precincts. Accept election date on the command line.

--- src/parser.py
+++ src/parser.py
@@ -27,47 +27,53 @@ import os
 from enum import Enum, auto
 import argparse
 import pandas
-
-OFFICE_CODES = {
-    0: "Total",
-    1: "President",
-    2: "Governor",
-    3: "Secretary of State",
-    4: "Attorney General",
-    5: "U.S. Senate",
-    6: "U.S. House",
-    7: "State Senate",
-    8: "State House"
-}
+import collections
 
 def main():
     args = parseArguments()
 
-    year = '2016' # TODO: Should infer from the path
-    parser = MIParser(year, args.stateDirPath, args.outPath)
+    parser = MIParser(args.stateDirPath, args.date, args.outDirPath)
     parser.writeOut()
 
 def parseArguments():
     parser = argparse.ArgumentParser(description='Parse Michigan SOS CSV files into OpenElections format')
     parser.add_argument('stateDirPath', metavar='MI_dir_path', type=str,
                         help='path to the directory with the Michigan CSV files for a given year')
-    parser.add_argument('outPath', type=str,
-                        help='path to the output CSV file')
+    parser.add_argument('date', type=str,
+                        help='the date of the election(s) in YYYYMMDD format')
+    parser.add_argument('outDirPath', type=str,
+                        help='path to output the CSV file(s) to')
+
 
     return parser.parse_args()
 
 class MIParser(object):
-    def __init__(self, year, stateDirPath, outPath):
-        self.year = year
+    OFFICE_CODES = {
+        1: "President",
+        2: "Governor",
+        3: "Secretary of State",
+        4: "Attorney General",
+        5: "U.S. Senate",
+        6: "U.S. House",
+        7: "State Senate",
+        8: "State House"
+    }
+
+
+    def __init__(self, stateDirPath, date, outDirPath):
         self.stateDirPath = stateDirPath
-        self.outPath = outPath
+        self.date = date
+        self.year = date[0:4]
+        self.state = 'mi'
+        self.outDirPath = outDirPath
         self.merged = None
-        self.results = []
+        self.results = collections.defaultdict(list)
 
         self.buildFileTable()
         self.readIn()
         self.process()
 
+    
     def readIn(self):
         def table(file):
             options = { 'header': None,                 # Data has no header row
@@ -85,24 +91,46 @@ class MIParser(object):
         # Sort into sensible order
         self.merged = df3.sort_values(['countyName', 'cityCode', 'precinct', 'officeCode', 'district'])
 
+    
     def process(self):
-        # This pseudo-office is the "Poll book totals"
-        # filtered = self.merged.loc[(self.merged.officeCode != 0)]
-
         for index, row in self.merged.iterrows():
-            if row['officeCode'] in OFFICE_CODES:
-                precinct = f"{row['cityName']} {row['precinct']}"
-                office = OFFICE_CODES[row['officeCode']]
+            if row['officeCode'] in MIParser.OFFICE_CODES:
+                precinct = row['precinct']
+
+                if precinct != 9999:
+                    precinct = f"{row['cityName'].title()} {row['precinct']}"
+
+                    label = row['precinctLabel'].strip()
+                    if label:
+                        if label == 'AVCB':
+                            precinct += " "
+
+                        precinct += f"{label}"
+
+                    if row['ward']:
+                        precinct += f" Ward {row['ward']}"
+                
+                office = MIParser.OFFICE_CODES[row['officeCode']]
                 district = row['district'][0:3]
                 party = row['party'].strip()
-                candidate = ' '.join(filter(None, [row['first'], row['middle'], row['last']])).strip() # exclude empty strings
+                candidate = ' '.join(filter(None, [row['first'], row['middle'], row['last']])).strip().title() # exclude empty strings
                 
                 if district == '000':
                     district = None
                 else:
                     district = int(district)
 
-                self.results.append([row['countyName'], precinct, office, district, party, candidate, row['votes']])
+                electionType = None
+                if row['status'] == 0:
+                    electionType = 'general'
+                elif row['status'] == 8:
+                    electionType = 'special'
+                
+                if not electionType:
+                    print(f"ERROR: Unknown status for vote: {row}")
+                else:
+                    result = [row['countyName'].title(), precinct, office, district, party, candidate, row['votes']]
+                    self.results[electionType].append(result)
         
 
     def buildFileTable(self):
@@ -117,11 +145,17 @@ class MIParser(object):
                         'cols': ['year', 'type', 'countyCode', 'cityCode', 'cityName']}
         }
 
+    
     def writeOut(self):
-        with open(self.outPath, 'w') as outfile:
+        for electionType, results in self.results.items():
+            path = os.path.join(self.outDirPath, f"{self.date}__{self.state}__{electionType}__precinct.csv")
+            self.writeResultsToFile(path, results)
+
+    def writeResultsToFile(self, outDirPath, results):
+        with open(outDirPath, 'w') as outfile:
             writer = csv.writer(outfile, lineterminator='\n')
             writer.writerow(['county', 'precinct', 'office', 'district', 'party', 'candidate', 'votes'])
-            writer.writerows(self.results)
+            writer.writerows(results)
 
 
 # Default function is main()
